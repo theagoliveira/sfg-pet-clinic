cf06228a007a02dbb7e3b877a7c0cb170331969d
Thiago Cavalcante <thiago.kun@gmail.com>
2021-05-07 05:25:50 -0300

    Add Vets index page and controller
    Resolves #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/petclinicweb/controllers/VetController.java
@@ -0,0 +1,15 @@
+package guru.springframework.petclinicweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+
+    @RequestMapping({"/", "", "/index", "/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+
+}
