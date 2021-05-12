d603b6cf87ed82112b634f875c16e37355b4f063
Thiago Cavalcante <thiago.kun@gmail.com>
2021-05-12 02:08:26 -0300

    Add Specialty entity and add to Vets
    Resolves #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Visit {
+public class Visit extends BaseEntity {
 
     private LocalDate date;
     private String description;
