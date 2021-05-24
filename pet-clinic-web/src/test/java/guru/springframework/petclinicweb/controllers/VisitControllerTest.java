fce184e15631601d879bb5d5700cbaea05dad086
Thiago Cavalcante <thiago.kun@gmail.com>
2021-05-24 16:41:39 -0300

    Improve VisitController tests with params

--- pet-clinic-web/src/test/java/guru/springframework/petclinicweb/controllers/VisitControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/petclinicweb/controllers/VisitControllerTest.java
@@ -2,6 +2,7 @@ package guru.springframework.petclinicweb.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -10,7 +11,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import java.util.HashSet;
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -31,7 +33,13 @@ import guru.springframework.sfgpetclinic.services.VisitService;
 class VisitControllerTest {
 
     private static final Long VISIT_ID = 1L;
-    private static final Visit VISIT = Visit.builder().id(VISIT_ID).build();
+    private static final LocalDate VISIT_DATE = LocalDate.now();
+    private static final String VISIT_DESCRIPTION = "descritpion";
+    private static final Visit VISIT = Visit.builder()
+                                            .id(VISIT_ID)
+                                            .date(VISIT_DATE)
+                                            .description(VISIT_DESCRIPTION)
+                                            .build();
 
     @Mock
     VisitService visitService;
@@ -67,7 +75,11 @@ class VisitControllerTest {
         when(petService.findById(anyLong())).thenReturn(Pet.builder().build());
         when(visitService.save(any(Visit.class))).thenReturn(VISIT);
 
-        mockMvc.perform(post("/owners/1/pets/1/visits"))
+        mockMvc.perform(
+            post("/owners/1/pets/1/visits").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                           .param("date", "2021-05-24")
+                                           .param("description", VISIT_DESCRIPTION)
+        )
                .andExpect(status().is3xxRedirection())
                .andExpect(view().name("redirect:/owners/1"))
                .andExpect(model().attributeExists("visit"))
@@ -75,6 +87,24 @@ class VisitControllerTest {
 
         verify(petService).findById(anyLong());
         verify(visitService).save(any(Visit.class));
+    }
+
+    @Test
+    void createVisitWithError() throws Exception {
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().build());
+
+        mockMvc.perform(
+            post("/owners/1/pets/1/visits").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                           .param("date", "123456789")
+                                           .param("description", VISIT_DESCRIPTION)
+        )
+               .andExpect(status().isOk())
+               .andExpect(view().name("owners/pets/visits/form"))
+               .andExpect(model().attributeExists("visit"))
+               .andExpect(model().attributeExists("pet"));
+
+        verify(petService).findById(anyLong());
+        verify(visitService, never()).save(any(Visit.class));
     }
 
 }
